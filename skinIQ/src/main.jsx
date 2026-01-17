241cbd8f2a4bb773923ab23fb752ea4fdc014236
NimsaraVTA <cisco1998learn@gmail.com>
2026-01-17 20:33:05 +0530

    remove unnecessary files and unnecessary lines

--- skinIQ/src/main.jsx
+++ skinIQ/src/main.jsx
@@ -1,6 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import SkinIQ from './SkinIQ.jsx'
 import "bootstrap/dist/css/bootstrap.min.css";
 
